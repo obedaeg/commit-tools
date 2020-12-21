3f006d8953d32f71fbbb969f061110b1aa34ba7f
Obed Espinoza <obed.espinoza@healthcare.com>
2020-12-21 15:06:19 -0600

    feat(workflow): installing semantic release
    We installed semantic release to check how it works and how the
    changelog is generated.

--- etl/main.py
+++ etl/main.py
@@ -9,12 +9,18 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def main():
+    print_hi(
+        'PyCharm ------------------- THIS IS A BIG LINE ---------------------------------------------------------------------------')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm ------------------- THIS IS A BIG LINE ---------------------------------------------------------------------------')
-    b = 0
-    c = 1
-    a = b + c
-    d = a + b
+    print_hi(
+        'PyCharm ------------------- THIS IS A BIG LINE ---------------------------------------------------------------------------')
+    bas = 0
+    cas = 1
+    ast = bas + cas
+    das = ast + bas
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
